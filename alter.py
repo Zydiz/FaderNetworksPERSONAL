0338f855b0c86dd797ca5667e6eb081e01e79c06
Zydiz <ardatangkr@gmail.com>
2023-06-10 21:39:10 +0300

    fix the output

--- alter.py
+++ alter.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import torch
 import librosa
+import librosa.display
 import soundfile as sf
 
 from src.logger import create_logger
@@ -16,49 +17,7 @@ melspec_params = {
     'fmin': 20
 }
 
-attr_keys = [('bright', 2), ('dark', 2), ('distortion', 2), ('percussive', 2)]
-
-# parse parameters
-parser = argparse.ArgumentParser(description='Attributes swapping')
-parser.add_argument("--model_path", type=str,
-                    help="Trained model path", required=True)
-parser.add_argument("--audio_path", type=str,
-                    help="Input audio path", required=True)
-parser.add_argument('--attrib_list', type=str,
-                    help='List of attributes in the form of ex. 1,1,0,1', required=True)
-parser.add_argument("--output_path", type=str, default="output.wav",
-                    help="Output path (.wav)")
-params = parser.parse_args()
-
-# check parameters
-assert os.path.isfile(params.model_path)
-assert params.output_path.lower().endswith('.wav')
-
-
-# TODO ATTR PARSING, check if worky
-attrib_list = [item for item in params.attrib_list.split(',')]
-attribs = {k[0]: np.zeros(1, dtype=np.bool_) for k in attr_keys}
-attribs["bright"]     = attrib_list[0]
-attribs["dark"]       = attrib_list[1]
-attribs["distortion"] = attrib_list[2]
-attribs["percussive"] = attrib_list[3]
-
-# # parse attributes
-# attrs = []
-# for name, n_cat in attr_keys:
-#     for i in range(n_cat):
-#         print(f"name = {name}, n_cat = {n_cat}, i = {i}")
-#         attrs.append(torch.FloatTensor((attribs[name] == i).astype(np.float32)))
-# attributes = torch.cat([x.unsqueeze(1) for x in attrs], 1)
-
-# float_attribs = {k: [float(v) for v in vals] for k, vals in attribs.items()}
-# attributes = torch.tensor(list(map(list, zip(*float_attribs.values()))))
-
-# create logger / load trained model
-logger = create_logger(None)
-ae = torch.load(params.model_path).eval()
-ae.n_attr = 4
-
+attr_keys = ['bright', 'dark', 'distortion', 'percussive']
 
 #############
 ### functions
@@ -82,6 +41,48 @@ def load_audio(params, file_path):
         #print(len(yt))
     return yt, sr
 
+# parse parameters
+parser = argparse.ArgumentParser(description='Attributes swapping')
+parser.add_argument("--model_path", type=str,
+                    help="Trained model path", required=True)
+parser.add_argument("--audio_path", type=str,
+                    help="Input audio path", required=True)
+parser.add_argument('--attrib_list', type=str,
+                    help='List of attributes in the form of ex. 1,1,0,1', required=True)
+parser.add_argument("--output_path", type=str, default="output.wav",
+                    help="Output path (.wav)")
+params = parser.parse_args()
+
+# check parameters
+assert os.path.isfile(params.model_path)
+assert params.output_path.lower().endswith('.wav')
+
+################
+### Attr Parsing
+################
+
+attrib_list = [item for item in params.attrib_list.split(',')]
+attribs = {k: 0 for k in attr_keys}
+attribs["bright"]     = int(attrib_list[0])
+attribs["dark"]       = int(attrib_list[1])
+attribs["distortion"] = int(attrib_list[2])
+attribs["percussive"] = int(attrib_list[3])
+
+# parse attributes
+attrs = []
+for name in attr_keys:
+    a_0 = 1 - attribs[name]
+    attrs.append(a_0)
+    a_1 = attribs[name]
+    attrs.append(a_1)
+
+attributes = torch.FloatTensor(attrs)
+attributes = attributes.unsqueeze(0)
+
+# create logger / load trained model
+logger = create_logger(None)
+ae = torch.load(params.model_path).eval()
+
 ######################
 ### Preprocessing step
 ######################
@@ -97,6 +98,7 @@ mels_n = np.array(mels)
 mels_n = np.expand_dims(mels_n, axis=1)
 
 data_m = torch.from_numpy(mels_n)
+
 # TODO check if true
 image = normalize_images(data_m.cpu())
 
@@ -105,11 +107,16 @@ image = normalize_images(data_m.cpu())
 #####################
 
 enc_outputs = ae.encode(image)
+# print(enc_outputs[2].size())
+print(enc_outputs[0].size(0))
 output = ae.decode(enc_outputs, attributes)[-1]
 
 #####################
 ### Reconstruct audio
 #####################
-output_np = output.numpy()
+output_np = output.detach().numpy()
+output_np = output_np.squeeze()
+print(f"output shape:{output_np.shape}")
 y_inv = librosa.feature.inverse.mel_to_audio(output_np, sr=sr, hop_length=((4*16000)//256+1))
+print(f"y shape:{y_inv.shape}")
 sf.write(params.output_path, y_inv, sr)
